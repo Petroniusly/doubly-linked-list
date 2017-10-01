655a883673293aa26f8440f7ba0081914ba599ab
Petroniusly <blinkovpag@gmail.com>
2017-10-01 23:55:33 +0300

    dev DLL without method "reverse"

--- src/linked-list.js
+++ src/linked-list.js
@@ -2,37 +2,132 @@ const Node = require('./node');
 
 class LinkedList {
     constructor() {
-        this.lenght = 0;
-        this.head = 0;
-        this.end = 0;
-
+        this.length = 0;
+        this._head;
+        this._tail;
     }
 
-    append(data) {}
+    append(data, previus) {
+        var node = new Node(data,0,0);
+        
+        if (this.isEmpty()) {
+            this._head = node;
+            this._tail = node;
+        } else {
+            previus = previus == undefined ? this._tail : previus; //if arg previus not comming, it equal this._tail
+            if (previus.next != 0) { //add between node in list;
+                previus.next.prev = node;
+                node.next = previus.next;
+                node.prev = previus;
+                previus.next = node;
+            } else { //add to the end
+                node.prev = previus;
+                previus.next = node;
+                this._tail = node; 
+            }
+            
+        }
+        this.length++;
+        //return this._head instanceof Node;
+    }
 
     head() {
-        return this.head;
+        return this._head.data;
     }
 
     tail() {
-        return this.end;
+        return this._tail.data;
     }
 
-    at(index) {}
+    at(index) {
+        var count = 0;
+        if (index >= this.length) {
+            return "Cannot find item. Index is bigger than list's length";
+        }
+        
+        //return " head: " + this._head.data + " next: " + this._head.next.data +"; tail: " + this._tail.data + "; list length = " + this.length;
+        return this.next(index, count, this._head).data;
+    }
 
-    insertAt(index, data) {}
+    // This Method search item by forward step from _head
+    next(index, count, node) { //add new method 'cause it needed when search in our list.
+        if (count === index) {
+            return node;
+        } else {
+            count++;
+            return this.next(index, count, node.next);
+        } 
+    }
+
+    insertAt(index, data) {
+        var count = 0;
+        if (index >= this.length) {
+            return "Cannot insert item. Index is bigger than list's length";
+        }
+        this.append(data, this.next(index - 1, count, this._head)); 
+    }
 
     isEmpty() {
-        return (this.lenght == 0);
+        return (this.length === 0);
     }
 
-    clear() {}
+    clear() {
+        this.length = 0;
+        this._head.data = null;
+        this._head.next = null;
+        this._tail = this._head;
+    }
 
-    deleteAt(index) {}
+    deleteAt(index) {
+        var count = 0;
+        if (index >= this.length) {
+            return "Cannot delete item. Index is bigger than list's length";
+        }
+        if (index == this.length - 1) { //last node in list
+            this._tail.prev.next = 0;
+            this._tail = this._tail.prev;
+            //update this._tail;
+        } else if (index == 0) { //first node in list
+            this._head.next.prev = 0;
+            this._head = this._head.next;
+        } else { 
+            var deleteNode = this.next(index, count, this._head);
+            deleteNode.next.prev = deleteNode.prev;
+            deleteNode.prev.next = deleteNode.next;
+        }
+    }
 
-    reverse() {}
+    reverse() {
+        var node = this._head;
+        var tempLink;
+        for (var i = 0; i < this.length; i++) {
+            tempLink = node.prev;
+            node.prev = node.next;
+            node.next = tempLink;
+            if (i == 0) {
+                this._tail = node;
+            }
+            if (i == this.length - 1) {
+                this._head = node;
+                break;
+            }
+            node = node.next;
+        }
+    }
 
-    indexOf(data) {}
+    indexOf(data) {
+        var node = this._head;
+        var count = 0;
+        while (count < this.length) {
+            if (node.data == data) {
+                return count;
+            } else {
+                node = node.next;
+                count++;
+            }
+        };
+        return -1;
+    }
 }
 
 module.exports = LinkedList;
